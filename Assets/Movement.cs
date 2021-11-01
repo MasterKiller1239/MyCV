a7b5a86ff84456bc65a7845d77dade0a188e5b20
Jakub <50194718+MasterKiller1239@users.noreply.github.com>
2021-11-01 17:55:38 +0100

    2 push

---
+++ Assets/Movement.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Movement : MonoBehaviour
+{
+    public NavMeshAgent agent;
+
+    public float rotateSpeedMovement =0.5f;
+    float rotateVelocity;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //agent = this.gameObject.GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(1))
+        {
+            RaycastHit hit;
+            if(Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition),out hit,Mathf.Infinity))
+            {
+                agent.SetDestination(hit.point);
+                Quaternion rotationToLookAt = Quaternion.LookRotation(hit.point - transform.position);
+                float rotationY = Mathf.SmoothDampAngle(transform.eulerAngles.y, rotationToLookAt.eulerAngles.y, ref rotateVelocity, rotateSpeedMovement * (Time.deltaTime * 5));
+            }
+
+        }
+    }
+}
